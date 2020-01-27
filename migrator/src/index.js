4f02b8f163219007a3d5e919193297a809d1d114
eliot <eliotvscott@gmail.com>
2020-01-27 16:25:29 -0400

    Initial setup

---
+++ migrator/src/index.js
@@ -0,0 +1,16 @@
+const DBMigrate = require('db-migrate');
+const dbm = DBMigrate.getInstance(true);
+
+function migrateUp(){
+    console.log('running migrations [up]...');
+    dbm.reset()
+        .then(() => {
+            dbm.up();
+            console.log('successfully ran migrations [up]...');
+        }).catch(err => {
+            console.error(err);
+    });
+}
+
+// Wait for database to start up, then run migrations
+setTimeout(migrateUp, 1000);
